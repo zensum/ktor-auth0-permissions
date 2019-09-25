aaf086b5fd38ce904d34f233b89c46a422586785
Anton Österberg <anton.osterberg@gmail.com>
2019-09-25 16:49:28 +0200

    Remove application configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version("1.3.50")
-    id("application") apply true
-}
-
-application {
-    mainClassName = "se.zensum.creditors.MainKt"
 }
 
 group = "se.zensum"
