f5868c7c0500da1cbb1c83fd8444059d137ba2ca
Anton Österberg <anton.osterberg@gmail.com>
2019-11-18 12:17:29 +0100

    Replace jackson-datatype-jsr310 with jackson-modules-java8

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     // Jackson
     implementation("com.fasterxml.jackson.core:jackson-core:2.9.10")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.10.1")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.10.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-modules-java8:2.9.10")
 
     // Testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
